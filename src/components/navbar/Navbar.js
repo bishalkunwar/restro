944266a84a98a0a0e771ee6ed73d266dc3d9b8c3
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-05 14:21:58 -0500

    creating and styling the navbar at very basic

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,20 @@
+import {HamburgerMenu} from "react-icons/gi";
+import {MdOutlineRestaurantMenu} from "react-icons/md";
+import "./Navbar.css";
 
 export default function Navbar(){
     return(
-        <p>Navbars</p>
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
+                <img src={MdOutlineRestaurantMenu} alt="logo imgs"/>
+            </div>
+            <ul className="app__navbar-links">
+                <li className="p__opensans"><a href="#home"></a>Home</li>
+                <li className="p__opensans"><a href="#about"></a>About</li>
+                <li className="p__opensans"><a href="#menu"></a>Menu</li>
+                <li className="p__opensans"><a href="#contact"></a>Contact</li>
+                <li className="p__opensans"><a href="#awards"></a>Awards</li>
+            </ul>
+        </nav>
     );
 };
