986ef329eb4779411dcf4abdecb9e74746c3a179
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-06 23:41:11 -0500

    creating the navbar for small screens

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -21,9 +21,22 @@ export default function Navbar(){
                 <a href="/" className="p__opensans">Book Table</a>   
             </div>
 
-            {/* <div className="app__navbar-smallscreen">
-                <GiHamburgerMenu color="#fff"/>
-            </div> */}
+            <div className="app__navbar-smallscreen">
+                <GiHamburgerMenu color="#fff" fontSize={27} onClick={()=>{}}/>
+
+                <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+                    <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={()=>{}}/>
+
+                </div>
+
+                <ul className="app__navbar-smallscreen-links">
+                    <li className="p__opensans"><a href="#home"></a>Home</li>
+                    <li className="p__opensans"><a href="#about"></a>About</li>
+                    <li className="p__opensans"><a href="#menu"></a>Menu</li>
+                    <li className="p__opensans"><a href="#contact"></a>Contact</li>
+                    <li className="p__opensans"><a href="#awards"></a>Awards</li>
+                </ul>
+            </div>
 
         </nav>
     );
