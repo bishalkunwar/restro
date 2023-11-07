cea5136b6407deb4867e218ba3ca4b8dc7259bad
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-07 00:17:41 -0500

    toggleevent to the small screen navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import {GiHamburgerMenu} from "react-icons/gi";
 import {MdOutlineRestaurantMenu} from "react-icons/md";
 import "./Navbar.css";
 
 export default function Navbar(){
+    const[toggle, setToggle] = useState(false);
+    
     return(
         <nav className="app__navbar">
             <div className="app__navbar-logo">
@@ -20,20 +23,21 @@ export default function Navbar(){
                 <div/>
                 <a href="/" className="p__opensans">Book Table</a>   
             </div>
-
-            <div className="app__navbar-smallscreen">
-                <GiHamburgerMenu color="#fff" fontSize={27} onClick={()=>{}}/>
-                <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-                    <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={()=>{}}/>
-                    <ul className="app__navbar-smallscreen-links">
-                        <li className="p__opensans"><a href="#home"></a>Home</li>
-                        <li className="p__opensans"><a href="#about"></a>About</li>
-                        <li className="p__opensans"><a href="#menu"></a>Menu</li>
-                        <li className="p__opensans"><a href="#contact"></a>Contact</li>
-                        <li className="p__opensans"><a href="#awards"></a>Awards</li>
-                    </ul>
+            {toggle && (
+                <div className="app__navbar-smallscreen">
+                    <GiHamburgerMenu color="#fff" fontSize={27} onClick={()=>{setToggle(true)}}/>
+                    <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+                        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={()=>{setToggle(false)}}/>
+                        <ul className="app__navbar-smallscreen-links">
+                            <li className="p__opensans"><a href="#home"></a>Home</li>
+                            <li className="p__opensans"><a href="#about"></a>About</li>
+                            <li className="p__opensans"><a href="#menu"></a>Menu</li>
+                            <li className="p__opensans"><a href="#contact"></a>Contact</li>
+                            <li className="p__opensans"><a href="#awards"></a>Awards</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            )}
 
         </nav>
     );
