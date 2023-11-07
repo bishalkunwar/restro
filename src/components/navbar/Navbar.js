a9fc1ccd1579aa364cd1c27c162f6b851356dde0
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-07 11:17:59 -0500

    toggle menu well implemented

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ import {MdOutlineRestaurantMenu} from "react-icons/md";
 import "./Navbar.css";
 
 export default function Navbar(){
-    const[toggle, setToggle] = useState(false);
+    const[toggling, setToggling] = useState(false);
     
     return(
         <nav className="app__navbar">
@@ -23,11 +23,12 @@ export default function Navbar(){
                 <div/>
                 <a href="/" className="p__opensans">Book Table</a>   
             </div>
-            {toggle && (
-                <div className="app__navbar-smallscreen">
-                    <GiHamburgerMenu color="#fff" fontSize={27} onClick={()=>{setToggle(true)}}/>
+            
+            <div className="app__navbar-smallscreen">
+                <GiHamburgerMenu color="#fff" fontSize={27} onClick={()=>{setToggling(true)}}/> 
+                {toggling && (
                     <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-                        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={()=>{setToggle(false)}}/>
+                        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={()=>{setToggling(false)}}/>
                         <ul className="app__navbar-smallscreen-links">
                             <li className="p__opensans"><a href="#home"></a>Home</li>
                             <li className="p__opensans"><a href="#about"></a>About</li>
@@ -36,8 +37,8 @@ export default function Navbar(){
                             <li className="p__opensans"><a href="#awards"></a>Awards</li>
                         </ul>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
 
         </nav>
     );
