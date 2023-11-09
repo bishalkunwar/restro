82141c426d11a5e91f829244883e3168e482e1ee
Bishal487 <bishal.kunwar487@gmail.com>
2023-11-09 12:06:16 -0500

    some stylings to the header

--- src/components/constants/Images.js
+++ src/components/constants/Images.js
@@ -1,6 +1,7 @@
 
 import spoon from "../assets/spoon.png";
+import welcome from "../assets/welcome.png";
 
 export default {
-    spoon
+    spoon, welcome
 };
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 
 import "./Header.css";
 import SubHeading from "../subHeading/SubHeading";
+import Images from "../constants/Images";
 
 export default function Header(){
 
@@ -11,10 +12,12 @@ export default function Header(){
                 <h1 className="app__header-h1">
                     The fine refined Nepalese Cuisine.
                 </h1>
+                <p className="p__opensans" style={{margin: "2rem 0", color: "white"}}> yes this is the fine refined nepalese tatese resturants with the availability of all the foods and soft drinks. we do serve very well with the customer relation concern always into our head. Thank You</p>
+                <button type="button" className="custom_button">Explore Menu</button>
             </div>
             
             <div className="app__wrapper_img">
-
+                <img src={Images.welcome} alt="header img" style={{ height: "80%", width: "90%"}} />
             </div>
         </div>
     );
